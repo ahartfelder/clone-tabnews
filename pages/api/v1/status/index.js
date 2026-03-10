1481d14de94418667942574f73a09fa2569ca853
Andreas Hartfelder <74719748+ahartfelder@users.noreply.github.com>
2026-03-10 19:52:55 +0000

    add `jsconfig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(req, res) {
   const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result);
+  console.log(result.rows);
   res.status(200).json({ message: "Status OK!" });
 }
 
