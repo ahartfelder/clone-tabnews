273754a71a109b689e17e5942e5b5bcc07aca380
Andreas Hartfelder <74719748+ahartfelder@users.noreply.github.com>
2026-03-16 18:24:46 +0000

    adds `wait-for-postgres.js` script

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,19 @@
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+  let count = 0;
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+
+  function handleReturn(error, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
+    }
+    console.log("\n\n🟢 Postgres está pronto e aceitando conexões!\n\n");
+  }
+}
+
+process.stdout.write("🔴 Aguardando Postgres aceitar conexões");
+
+checkPostgres();
